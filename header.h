091bc8a70c925eabee908fbfded7184163d33bfb
luke36 <luxy1115@yahoo.com>
2016-10-15 16:46:11 +0800

    Update header.h
    Delete some unuseful definitions

--- header.h
+++ header.h
@@ -9,25 +9,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define EMPTY 0
-#define NOT_MOVED -1
-#define DEFAULT_TABLE_WID 4
-#define DEFAULT_TABLE_LEN 4
-#define SCREEN_WID 30
-#define CELL_WID (SCREEN_WID / wid)
-#define CELL_LEN (CELL_WID / 2)
-#define SHELL ">>> "
-#define USAGE "2048 [width] [length]"
+#define RAND_NUM (rand() % 2 ? 2 : 4)
 
 enum dir { DOWN, UP, RIGHT, LEFT };
-enum dir_key { KEY_DOWN = 's', KEY_UP = 'w', KEY_RIGHT = 'd', KEY_LEFT = 'a', KEY_EXIT = 'q' };
 
 extern int wid;
 extern int len;
 
-void add_num(int [len][wid]);
-void init_table(int [len][wid]);
-int is_full(const int [len][wid]);
-int is_mergeable(const int [len][wid]);
-int merge_table(int [len][wid], enum dir);
-void print_table(const int [len][wid], int, int, char, char, char);
+void add_num(int [len][wid], int num);
